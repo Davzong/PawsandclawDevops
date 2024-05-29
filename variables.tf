a2da020b2e9cc7a2c06cd431bc9735b8e3da49a4
syukay <syukay00789@gmail.com>
2024-05-29 18:07:46 +1000

    commit modules of terraform

--- variables.tf
+++ variables.tf
@@ -1,12 +1,5 @@
 # variables.tf
 
-variable "aws_access_key" {
-    description = "The IAM public access key"
-}
-
-variable "aws_secret_key" {
-    description = "IAM secret access key"
-}
 
 variable "aws_region" {
     description = "The AWS region things are created in"
