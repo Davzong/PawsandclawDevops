7f6e8a713e22b73811ae54956f0af1e3cb79df5b
syukay <syukay00789@gmail.com>
2024-06-03 13:01:50 +1000

    feat: update the s3.tf

--- s3.tf
+++ s3.tf
@@ -2,10 +2,10 @@ resource "aws_s3_bucket" "this" {
   bucket = var.s3_name
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   acl    = "private"
+# }
 
 resource "aws_s3_bucket_website_configuration" "this" {
   bucket = aws_s3_bucket.this.bucket
@@ -36,4 +36,13 @@ resource "aws_s3_bucket_policy" "this" {
       }
     ]
   })
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls   = false
+  ignore_public_acls  = false
+  block_public_policy = false
+  restrict_public_buckets = false
 }
